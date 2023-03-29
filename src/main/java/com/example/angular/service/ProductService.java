f9dedeecfd724464c5b976ac0fe7bbe4f64d3e0c
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-03-29 16:20:16 -0300

    Service

---
+++ src/main/java/com/example/angular/service/ProductService.java
@@ -0,0 +1,58 @@
+package com.example.angular.service;
+
+
+import com.example.angular.controller.dto.ProductPostReqBody;
+import com.example.angular.controller.dto.ProductPutReqBody;
+import com.example.angular.persistence.Product;
+import com.example.angular.repository.ProductRepository;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product save(final ProductPostReqBody productPostReqBody) {
+        val product = Product.builder()
+                .id(productPostReqBody.getId())
+                .name(productPostReqBody.name)
+                .category(productPostReqBody.getCategory())
+                .build();
+        return productRepository.save(product);
+    }
+
+    public Product update(final ProductPutReqBody productPutReqBody) {
+        val byId = productRepository.findById(productPutReqBody.getId());
+
+        if (byId.isEmpty()) {
+            throw new IllegalArgumentException("ID inexistente!");
+        }
+
+        val product = Product.builder()
+                .id(productPutReqBody.getId())
+                .name(productPutReqBody.getName())
+                .category(productPutReqBody.getCategory())
+                .build();
+        return productRepository.save(product);
+    }
+
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(final Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("Customize exception!"));
+    }
+
+    public void delete(final Long id) {
+        productRepository.deleteById(id);
+    }
+}
