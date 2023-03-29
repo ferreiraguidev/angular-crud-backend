e096d1e4c960010867e8618eb8371938b7a391fb
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-03-29 16:19:32 -0300

    connecting with JpaRepository

---
+++ src/main/java/com/example/angular/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.angular.repository;
+
+
+import com.example.angular.persistence.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
